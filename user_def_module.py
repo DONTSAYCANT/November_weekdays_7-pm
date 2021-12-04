d90a5ab5ca9e57362d0321c305eaef712b289b65
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2021-12-04 18:16:45 +0530

    Add files via upload

---
+++ user_def_module.py
@@ -0,0 +1,10 @@
+def func1(val1, val2):
+    val3 = val1 + val1[::-1]
+    return val3
+
+def func2(int1, int2, int3):
+    return (int1 + int2)*(int3+int2)
+
+
+def func3(int1, int2, int3):
+    return (int1 + int2)*(int3+int2)
